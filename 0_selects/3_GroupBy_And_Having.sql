48983e7bfc9542ef553ad601a6b365dc54b621a5
Anh Tong <tongkimanh@yahoo.com>
2021-09-29 19:11:07 +0000

    Practise group by and having queries

---
+++ 0_selects/3_GroupBy_And_Having.sql
@@ -0,0 +1,16 @@
+/* GROUP BY */
+SELECT students.name as student,
+count(assignment_submissions.id) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
+
+/* HAVING */
+SELECT students.name as student,
+count(assignment_submissions.id) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.id) < 100;
