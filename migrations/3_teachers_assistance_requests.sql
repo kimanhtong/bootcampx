90e2b8f8583bb05107774e8600eca4e76731dbee
Anh Tong <tongkimanh@yahoo.com>
2021-09-29 23:08:06 +0000

    Add teachers and assistance_requests tables

---
+++ migrations/3_teachers_assistance_requests.sql
@@ -0,0 +1,22 @@
+DROP TABLE assistance_requests;
+DROP TABLE teachers;
+
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE,
+  is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
